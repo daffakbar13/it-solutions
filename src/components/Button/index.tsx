09751364add73a8fec66a1cf2329b382a24b0a4b
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-17 15:38:25 +0700

    change component button to export default

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import { Button as ButtonMui, styled } from '@mui/material'
 
-export const Button = styled(ButtonMui)(() => ({
+const Button = styled(ButtonMui)(() => ({
   textTransform: 'none',
 }))
+
+export default Button
