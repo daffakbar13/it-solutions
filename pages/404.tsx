bdf1413239662030c71d9fdbe502b6ad68fa5cb0
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-17 15:56:59 +0700

    change import page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import PageNotFound from 'src/containers/PageNotFound'
+import { PageNotFound } from 'src/containers'
 
 const NotFound: NextPage = () => <PageNotFound />
 
