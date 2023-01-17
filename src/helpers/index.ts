99f3f7a5d845ae3e76e172325694a94a08047e9a
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-17 15:49:21 +0700

    change helper export * to export default as

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
-export * from './createSubMenu'
-export * from './createMenu'
+export { default as createPath } from './createPath'
+export { default as createMenu } from './createMenu'
+export { default as getPath } from './getPath'
