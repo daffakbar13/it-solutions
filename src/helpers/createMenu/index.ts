c518a560f47945b44239995fa24bc65d8ca6bb5d
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-17 15:44:20 +0700

    update helper createMenu

--- src/helpers/createMenu/index.ts
+++ src/helpers/createMenu/index.ts
@@ -1,6 +1,15 @@
-import { createSubMenu } from '../createSubMenu'
+import { paths } from 'src/configs'
+import { MenuTypes } from 'src/shared'
 
-export const createMenu = (title: string, subMenu: ReturnType<typeof createSubMenu>[]) => ({
-  title,
-  subMenu,
-})
+export default function createMenu(key: MenuTypes) {
+  return {
+    title: key,
+    subMenu: Object.entries(paths)
+      .filter((p) => p[1].parent === key)
+      .map((p) => {
+        const [, subMenu] = p
+        const { name, path } = subMenu
+        return { name, path }
+      }),
+  }
+}
