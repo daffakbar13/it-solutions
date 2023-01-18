922b41565cb0f8d421a03201c223bd78e6e06f38
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2023-01-18 14:06:05 +0700

    update component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,23 @@
-import { Button as ButtonMui, styled } from '@mui/material'
+import { Button as ButtonMui, ButtonProps, styled } from '@mui/material'
 
-const Button = styled(ButtonMui)(() => ({
-  textTransform: 'none',
-}))
+const Button = styled((props: ButtonProps) => <ButtonMui disableElevation {...props} />)(
+  ({ theme, variant }) => ({
+    textTransform: 'none',
+    ...(variant === 'contained' && {
+      backgroundColor: 'black',
+      color: 'white',
+      ':hover': {
+        backgroundColor: theme.palette.primary.main,
+      },
+    }),
+    ...((variant === 'text' || !variant) && {
+      color: 'black',
+      ':hover': {
+        backgroundColor: 'transparent',
+        color: theme.palette.primary.main,
+      },
+    }),
+  }),
+)
 
 export default Button
